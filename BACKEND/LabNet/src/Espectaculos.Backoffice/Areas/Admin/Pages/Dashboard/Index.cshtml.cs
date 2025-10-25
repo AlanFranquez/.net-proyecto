9f4e1fbc9199c1466917bfe65e54b0f14e01759d
Jx <134028082+Decodexj@users.noreply.github.com>
2025-10-25 19:56:19 -0300

    Backoffice Placeholder

---
+++ BACKEND/LabNet/src/Espectaculos.Backoffice/Areas/Admin/Pages/Dashboard/Index.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Espectaculos.Backoffice.Areas.Admin.Pages.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
---
+++ BACKEND/LabNet/src/Espectaculos.Backoffice/Areas/Admin/Pages/Espacios/Index.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Espectaculos.Backoffice.Areas.Admin.Pages.Espacios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
