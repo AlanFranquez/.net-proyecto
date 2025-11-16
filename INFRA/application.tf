abfa359b76abb329f922699e54a9c2f7a619d8b9
Fabricio Fuentes <fuentesfabricio17@gmail.com>
2025-11-16 19:06:05 -0300

    ci

--- INFRA/application.tf
+++ INFRA/application.tf
@@ -85,3 +85,47 @@ resource "aws_eks_node_group" "default" {
   instance_types = ["t3.medium"]
 }
 
+# Obtener nombre del AutoScaling Group que crea el NodeGroup
+data "aws_autoscaling_groups" "eks_asgs" {
+  filter {
+    name   = "tag:eks:cluster-name"
+    values = [aws_eks_cluster.main.name]
+  }
+}
+
+data "aws_autoscaling_group" "eks_group" {
+  name = element(data.aws_autoscaling_groups.eks_asgs.names, 0)
+}
+
+# Obtener instancias (si existen)
+data "aws_instances" "eks_nodes" {
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+
+  filter {
+    name   = "tag:aws:autoscaling:groupName"
+    values = [data.aws_autoscaling_group.eks_group.name]
+  }
+}
+
+# Adjuntar instancias al target group, usando desired_capacity como fallback
+resource "aws_lb_target_group_attachment" "nodes" {
+  count = (
+    length(data.aws_instances.eks_nodes.ids) != 0
+    ? length(data.aws_instances.eks_nodes.ids)
+    : data.aws_autoscaling_group.eks_group.desired_capacity
+  )
+
+  target_group_arn = aws_lb_target_group.eks_tg.arn
+  target_id        = element(
+    coalesce(data.aws_instances.eks_nodes.ids, ["dummy"]),
+    count.index
+  )
+  port             = 30080
+
+  depends_on = [
+    aws_eks_node_group.default
+  ]
+}
